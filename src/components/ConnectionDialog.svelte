70c17f6b02595743c8674eefc87d9aed1dbc7476
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2024-10-29 19:43:56 +0100

    project setup

---
+++ src/components/ConnectionDialog.svelte
@@ -0,0 +1,11 @@
+<p>Connection dialog</p>
+
+<script>
+
+</script>
+
+<style>
+	p {
+		@apply text-red-600;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import ConnectionDialog from '../components/ConnectionDialog.svelte';
+
+	let number = $state(1);
+	let prod = $derived(number * 5)
+</script>
+
+<h1>Power meter</h1>
+<input type="range" bind:value={number}/>
+<p>{prod}</p>
+<ConnectionDialog/>
