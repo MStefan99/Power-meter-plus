0875b9950834c4aa85ccfc7e3529f967dce6fd8a
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2024-10-31 22:58:33 +0100

    implemented digits

--- src/components/SevenSegment.svelte
+++ src/components/SevenSegment.svelte
@@ -1,5 +1,19 @@
-<script>
-	// let { digit } = $props();
+<script lang="ts">
+	let { digit }: { digit: number } = $props();
+
+	const table = [
+		[1, 1, 1, 1, 1, 1, 0],
+		[0, 1, 1, 0, 0, 0, 0],
+		[1, 1, 0, 1, 1, 0, 1],
+		[1, 1, 1, 1, 0, 0, 1],
+		[0, 1, 1, 0, 0, 1, 1],
+		[1, 0, 1, 1, 0, 1, 1],
+		[1, 0, 1, 1, 1, 1, 1],
+		[1, 1, 1, 0, 0, 0, 0],
+		[1, 1, 1, 1, 1, 1, 1],
+		[1, 1, 1, 1, 0, 1, 1]
+	];
+	const error = [1, 0, 0, 1, 1, 1, 1];
 
 	const segments = String.fromCharCode(
 		...new Array(7).fill(0).map((v, i) => i + 'a'.charCodeAt(0))
@@ -7,21 +21,24 @@
 </script>
 
 <div class="digit">
-	{#each segments as segment}
-		<div class="segment {segment}"></div>
+	{#each segments as segment, i}
+		<div class="segment {segment}" class:lit={table[digit]?.[i] ?? error[i]}></div>
 	{/each}
 </div>
 
 <style>
 	.digit {
 		--height: 100px;
-		--segment-width: 10px;
+		--segment-width: var(--segment, calc(var(--height) / 10));
 
 		--s-long: calc((var(--height) - 3 * var(--segment-width)) / 2);
 		--s-short: var(--segment-width);
-		--bg: var(--color, #0f0);
+
+		--bg: var(--color, #030);
+		--bg-lit: var(--color-lit, #0f0);
 		--digit-skew: -8deg;
 
+		display: inline-block;
 		transform: skewX(var(--digit-skew));
 		height: var(--height);
 		width: calc(var(--s-long) + 2 * var(--s-short));
@@ -29,9 +46,14 @@
 
 	.segment {
 		background-color: var(--bg);
-		border-radius: 2px;
+		border-radius: calc(var(--segment-width) / 4);
 		position: absolute;
-		box-shadow: 0 0 calc(var(--segment-width) / 2) calc(0px - var(--segment-width) / 8) var(--bg);
+	}
+
+	.segment.lit {
+		background-color: var(--bg-lit);
+		box-shadow: 0 0 min(calc(var(--segment-width)), 36px) calc(0px - var(--segment-width) / 6)
+			var(--bg-lit);
 	}
 
 	.segment.a,
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,12 +3,18 @@
 	import ConnectionDialog from '../components/ConnectionDialog.svelte';
 	import PowerView from '../components/PowerView.svelte';
 	import SevenSegment from '../components/SevenSegment.svelte';
+
+	let digit = 0;
+
+	setInterval(() => (digit = (digit + 1) % 10), 1000);
 </script>
 
 <div class="home">
 	{#if !getActiveDevice()}
 		<ConnectionDialog />
-		<SevenSegment digit={2} />
+		<div class="m-24">
+			<SevenSegment {digit} />
+		</div>
 	{:else}
 		<PowerView />
 	{/if}
