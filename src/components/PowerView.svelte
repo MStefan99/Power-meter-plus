1efa736a61f40d9d1f3c09f01187990c127fb7e8
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2024-11-04 11:18:35 +0200

    wip: power display

--- src/components/PowerView.svelte
+++ src/components/PowerView.svelte
@@ -1,19 +1,56 @@
 <script lang="ts">
-	import { disconnectDevice } from '$lib/driver/driver.svelte';
+	import { disconnectDevice, getActiveDevice } from '$lib/driver/driver.svelte';
+	import NumberDisplay from './NumberDisplay.svelte';
 
-	// $effect(() => {
-	// 	const interval = setInterval(() => {
-	// 		console.log('Interval', interval, getActiveDevice());
-	// 		getActiveDevice()?._request('*IDN?');
-	// 	}, 1000);
-	//
-	// 	return () => {
-	// 		clearInterval(interval);
-	// 	}
-	// });
+	const historyEntries = 60; // History entries to keep
+	const historyLength = 30; // History duration in seconds
+
+	let voltageHistory: number[] = $state([]);
+	let currentHistory: number[] = $state([]);
+	let powerHistory: number[] = $state([]);
+
+	$effect(() => {
+		const interval = setInterval(
+			() => {
+				if (getActiveDevice() !== null) {
+					voltageHistory.push(+getActiveDevice()._request('MEAS:VOLT?'));
+					currentHistory.push(+getActiveDevice()._request('MEAS:CURR?'));
+					powerHistory.push(+getActiveDevice()._request('MEAS:POW?'));
+
+					voltageHistory = voltageHistory.slice(voltageHistory.length - historyEntries);
+					currentHistory = currentHistory.slice(currentHistory.length - historyEntries);
+					powerHistory = powerHistory.slice(powerHistory.length - historyEntries);
+				}
+			},
+			(1000 * historyLength) / historyEntries
+		);
+
+		return () => {
+			clearInterval(interval);
+		};
+	});
 </script>
 
 <div class="power-view">
 	<p>Power view</p>
+	<NumberDisplay
+		value={voltageHistory[voltageHistory.length - 1]}
+		integerDigits={2}
+		decimalDigits={3}
+		--color="#300"
+		--color-lit="#f00"
+	/>
+	<NumberDisplay
+		value={currentHistory[currentHistory.length - 1]}
+		integerDigits={2}
+		decimalDigits={3}
+	/>
+	<NumberDisplay
+		value={powerHistory[powerHistory.length - 1]}
+		integerDigits={3}
+		decimalDigits={3}
+		--color="#330"
+		--color-lit="#ff0"
+	/>
 	<button onclick={disconnectDevice}>Disconnect</button>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,22 +2,13 @@
 	import { getActiveDevice } from '$lib/driver/driver.svelte';
 	import ConnectionDialog from '../components/ConnectionDialog.svelte';
 	import PowerView from '../components/PowerView.svelte';
-	import NumberDisplay from '../components/NumberDisplay.svelte';
-
-	// setInterval(() => (number = (number - 0.1234)), 200);
 </script>
 
 <div class="home">
 	{#if !getActiveDevice()}
 		<ConnectionDialog />
-		<div class="display-frame-1">
-			<div class="display-frame-2">
-				<div class="display p-4">
-					<NumberDisplay value="-1.1234" integerDigits={3} decimalDigits={2} />
-				</div>
-			</div>
-		</div>
-	{:else}
-		<PowerView />
 	{/if}
+	<div class="ml-4">
+		<PowerView />
+	</div>
 </div>
